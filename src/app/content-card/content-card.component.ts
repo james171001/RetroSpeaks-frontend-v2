ab50ad31718058aa7efff67ad90993dc04086258
James <jameserne9@gmail.com>
2023-07-03 17:36:41 +0800

    integrated some UI's center homepage, also added buttons

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -6,8 +6,25 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./content-card.component.css']
 })
 export class ContentCardComponent {
-  @Input()
-  title!: string;
-  @Input()
-  description!: string;
+  @Input() title!: string;
+  @Input() description!: string;
+  @Input() commentCount!: number;
+
+  get timePassed(): string {
+    const createdDate = new Date(); // Replace this with the actual creation date of the post
+    const currentTime = new Date();
+    const timeDifference = Math.abs(currentTime.getTime() - createdDate.getTime());
+    const minutesPassed = Math.floor(timeDifference / (1000 * 60));
+  
+    if (minutesPassed < 1) {
+      const secondsPassed = Math.floor(timeDifference / 1000);
+      return `${secondsPassed} seconds ago`;
+    } else if (minutesPassed < 60) {
+      return `${minutesPassed} minutes ago`;
+    } else {
+      const hoursPassed = Math.floor(timeDifference / (1000 * 60 * 60));
+      return `${hoursPassed} hours ago`;
+    }
+  }
+  
 }
