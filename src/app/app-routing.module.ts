52d51d6f9ba6196dad44c10129447b76ba2581c4
James <jameserne9@gmail.com>
2023-07-04 11:05:15 +0800

    added forgot-password and some bg changes to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { RegisterComponent } from './register/register.component';
 import { CreatePostComponent } from './create-post/create-post.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path:'login', component:LoginComponent},
+  {path:'forgot-password', component:ForgotPasswordComponent},
   {path:'error', component: ErrorComponent},
   { path: 'homepage', loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule) },
   {path:'create-post', component:CreatePostComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomepageModule } from './homepage/homepage.module';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorComponent } from './error/error.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { MatCardModule } from '@angular/material/card';
-import {
-  
-  HttpClientModule
-} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import { AuthService } from './services/auth.service';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-
-
 
 @NgModule({
   declarations: [
