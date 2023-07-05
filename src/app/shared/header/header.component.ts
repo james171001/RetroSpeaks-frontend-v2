ced77adb191e7fabb18f6d415daf336b932578ac
James <jameserne9@gmail.com>
2023-07-05 17:19:10 +0800

    added search box on header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  searchQuery: string = '';
 
+  resetSearch() {
+    this.searchQuery = '';
+  }
+
+  performSearch() {
+    // Implement your search functionality here
+    console.log('Performing search:', this.searchQuery);
+  }
 }
