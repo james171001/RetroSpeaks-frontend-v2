3968c6cb94c57eaa15d6671fc04b30e11bd6e739
James <jameserne9@gmail.com>
2023-07-06 10:18:14 +0800

    revise the way feed page scrollable

--- src/app/homepage/homepage-feed/homepage-feed.component.ts
+++ src/app/homepage/homepage-feed/homepage-feed.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ContentCardComponent } from 'src/app/shared/content-card/content-card.component';
 
 @Component({
@@ -24,6 +24,21 @@ export class HomepageComponent {
   isFeedComponentVisible() {
     return this.showFeedComponent && !this.showCreatePostComponent;
   }
-}
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.toggleBackToTopButton();
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  private toggleBackToTopButton() {
+    const button = document.querySelector('.back-to-top-button') as HTMLElement;
+    if (button) {
+      button.style.display = window.pageYOffset > 200 ? 'block' : 'none';
+    }
+  }
+}
 
