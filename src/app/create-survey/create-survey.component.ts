d76990ae7fe6a8061edf79d300c708c776d220e8
uhleesuuuh <espiritu.alisa@gmail.com>
2023-07-10 16:58:46 +0800

    added functionality in creating survey

--- src/app/create-survey/create-survey.component.ts
+++ src/app/create-survey/create-survey.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
 
+interface AnswerOption {
+  text: string;
+  selected?: boolean;
+}
+
+interface Question {
+  question: string;
+  answerType: string;
+  answerOptions?: AnswerOption[];
+  selectedOption?: number;
+  minValue?: string;
+  maxValue?: string;
+}
+
 @Component({
   selector: 'app-create-survey',
   templateUrl: './create-survey.component.html',
-  styleUrls: ['./create-survey.component.css']
+  styleUrls: ['./create-survey.component.css'],
 })
 export class CreateSurveyComponent {
+  surveyTitle!: string;
+  questions: Question[] = [];
 
+  addQuestion() {
+    this.questions.push({
+      question: '',
+      answerType: 'multiple-choice',
+      answerOptions: [],
+    });
+  }
+
+  removeQuestion(index: number) {
+    this.questions.splice(index, 1);
+  }
+
+  addAnswerOption(question: Question) {
+    if (!question.answerOptions) {
+      question.answerOptions = [];
+    }
+    question.answerOptions.push({ text: '', selected: false });
+  }
+
+  removeAnswerOption(question: Question, index: number) {
+    if (question.answerOptions) {
+      question.answerOptions.splice(index, 1);
+    }
+  }
+
+  resetAnswerOptions(question: Question) {
+    question.answerOptions = [];
+  }
+
+  submitSurvey() {
+    console.log('Title:', this.surveyTitle);
+    console.log('Questions:', this.questions);
+  }
 }
