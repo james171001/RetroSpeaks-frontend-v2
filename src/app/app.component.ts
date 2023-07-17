247dccd7c3961ce9f49c7e2db89ad58dbb35aa15
James <jameserne9@gmail.com>
2023-07-17 16:40:44 +0800

    revised dark mode and fixed UI for edit profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Renderer2, ElementRef, HostListener } from '@angular/core';
 import { ThemeService } from './shared/services/theme.service';
 
 @Component({
@@ -9,8 +9,10 @@ import { ThemeService } from './shared/services/theme.service';
 export class AppComponent {
   isDarkMode = false;
   rotationDirection = 0;
+  scrollOffset = 400;
+  showBackToTopButton = false;
 
-  constructor(private themeService: ThemeService) {
+  constructor(private themeService: ThemeService,private renderer: Renderer2, private elementRef: ElementRef) {
     this.themeService.themeChangeEmitter.subscribe(isDarkMode => {
       this.isDarkMode = isDarkMode;
     });
@@ -22,4 +24,27 @@ export class AppComponent {
   toggleRotation() {
     this.themeService.toggleRotation();
   }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.toggleBackToTopButton();
+  }
+
+  toggleBackToTopButton() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const button = this.elementRef.nativeElement.querySelector('.back-to-top-button');
+    const showScrollThreshold = 650; 
+
+    if (scrollPosition > showScrollThreshold) {
+      this.renderer.setStyle(button, 'display', 'block');
+    } else {
+      this.renderer.setStyle(button, 'display', 'none');
+    }
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+
 }
