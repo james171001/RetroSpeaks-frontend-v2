4037873ec06b624df4d82d39805c99ac5370e262
uhleesuuuh <espiritu.alisa@gmail.com>
2023-07-18 12:07:01 +0800

    changed token for News API

--- src/app/shared/news-content/news-api.service.ts
+++ src/app/shared/news-content/news-api.service.ts
@@ -9,8 +9,8 @@ export class NewsApiService {
 
   constructor(private http:HttpClient) { }
 
-  // topHeadLinesApiUrl = 'https://newsapi.org/v2/top-headlines?country=ph&apiKey=f1007b267c0c461db9a3f8a8a1a01f99' //News in Philippines
-  topHeadLinesApiUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=f1007b267c0c461db9a3f8a8a1a01f99'  //News in US
+  // topHeadLinesApiUrl = 'https://newsapi.org/v2/top-headlines?country=ph&apiKey=f5918cc4f2dd426882daadaec9065a33' //News in Philippines
+  topHeadLinesApiUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=f5918cc4f2dd426882daadaec9065a33'  //News in US
 
   topHeadLines(): Observable<any> {
     return this.http.get(this.topHeadLinesApiUrl);
